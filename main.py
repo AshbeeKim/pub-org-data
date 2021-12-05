331d62d5d2018f77f3d2a7cca61feb4ea2c92d22
AshbeeKim <co_m@gnu.ac.kr>
2021-12-05 22:38:30 +0900

    :construction:-----:hammer_and_wrench:define_v0.0.2:hammer_and_wrench:-----:construction:

--- main.py
+++ main.py
@@ -10,24 +10,26 @@ import pandas as pd
 # ~~~~~~~~~~~~~
 반복 작업의 구간을 각 모듈별로 처리되도록 하려면,
 '''
-print('+'*50, '\n\n\n')
+print(f"\n\n\t{'+'*70}\t\n")
 
 if __name__ == '__main__':
+    # os.getcwd=='/Volumes/WORK/OpenData'
     while True:
         task_name = input('Task : ')
         if task_name=='compare':    # 비교 업무 진행 시, # script로 생성한 파일 옮겨야 함.
-            # from config.compare import Compare
-            # 바로 이어지고 받아지도록 하려면, Compare 안에도 main.py가 있어야 함.
-            # os.getcwd=='/Volumes/WORK/OpenData'
+            from config import compare
             # import script.config.repeatContents as IC
-            print(IC.NessFromJSON(f'{os.getcwd()}/{task_name}')[0])
 
         elif task_name == 'define': # 정의 업무 진행 시
-            print(f"\n\n{'='*50} define {'='*50}\n\n")
+            print(f"\n\n\t {'='*30} define {'='*30}\t\n\n")
             from config import define
             # import define만 해도 바로 조회가 가능하도록, config.define.__init__.py 작성
+            # 우선 각 테이블로 다를 수 있을 것 같아서, 세부 내용을 확인하겠냐는 질문을 
+            # ./config/define/__init__.py에 작성했으나, 내용을 자세히 확인할 경우도 가정하기
             
             while True: # 값 비교, 추가 확인 등의 데이터 세부 내용 확인이 필요한 경우
+                # checkups가 script, scraping에서도 반복된다면, 그냥 ./config/checkups.py로 만들기
+                # 우선은 보류
                 checkups = input("내용을 자세히 확인하겠습니까?(True: 1, False: 0)\t")
                 
                 try:
@@ -45,8 +47,10 @@ if __name__ == '__main__':
                 if checkups:
                     from config.define import kr_lens
                     from config.define import kr_unqs
+                    # 어떤 경우에 내용을 자세히 확인해야 했던가를 더 고려해서 작성해야할 듯
                     
-            print(f"\n\n{'='*50} finished {'='*50}\n\n")
-        
-        # another_check = input(f"추가적으로 확인할 사항이 있습니까?\t")    # script랑 연결할 때 사용
+            print(f"\n\n\t{'='*30} finished {'='*30}\t\n\n")
+        # another_check = input(f"추가적으로 확인할 사항이 있습니까?\t")
+        # if another_check  
+        # script랑 연결할 때 사용, script가 아니더라도 define에서 단어의 용례를 검색하는 것에 활용해도 좋을 듯?
         break
